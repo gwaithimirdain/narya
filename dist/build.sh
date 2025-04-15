4b1cf095e1f05fc1990dcaf5882bd528c465ba72
Michael Shulman <shulman@sandiego.edu>
2025-04-15 16:13:33 -0700

    add installation instructions to static build release

--- dist/build.sh
+++ dist/build.sh
@@ -4,6 +4,7 @@ NAME=narya-`git show -s --format=%h`-`date +'%Y%m%d'`
 mkdir -p $NAME
 cp ../proofgeneral/*.el $NAME
 cp ../result/bin/narya $NAME
+cp INSTALL.txt $NAME
 tar -czf $NAME.tar.gz $NAME
 mkdir -p build/releases
 mv $NAME.tar.gz build/releases
