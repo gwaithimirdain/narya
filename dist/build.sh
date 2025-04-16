79de322a54ddba5c7736d801cb193f1c2bce552a
Michael Shulman <shulman@sandiego.edu>
2025-04-15 23:48:49 -0700

    oops, include install script in static build tarball

--- dist/build.sh
+++ dist/build.sh
@@ -4,7 +4,7 @@ NAME=narya-`git show -s --format=%h`-`date +'%Y%m%d'`
 mkdir -p $NAME
 cp ../proofgeneral/*.el $NAME
 cp ../result/bin/narya $NAME
-cp INSTALL.txt $NAME
+cp INSTALL.txt install-pg.sh proof-site.patch $NAME
 tar -czf narya-master-static.tar.gz $NAME
 mkdir -p build/releases
 mv narya-master-static.tar.gz build/releases
